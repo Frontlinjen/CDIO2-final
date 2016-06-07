be9dbfe9fcea818acd9e28603721a05819904bec
SenadBegovic <senadbegovic2500@hotmail.com>
2016-06-07 16:07:37 +0200

    Fixed methods and changes in all classes related to ProduktBatches

--- src/cdiofinal/server/ProduktBatchDAO.java
+++ src/cdiofinal/server/ProduktBatchDAO.java
@@ -9,4 +9,5 @@ public interface ProduktBatchDAO {
 	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
 	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+	void deleteProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 }
