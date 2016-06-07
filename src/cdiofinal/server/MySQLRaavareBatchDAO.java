73f80adbe9e37e8d7a635d4e8273b1c25c3f0d78
MikkelLeth <MikkelLeth@10.16.170.46>
2016-06-07 22:00:31 +0200

    DAO's for RaavareBtch and Rank
    Created the methods that was implemented from the interfaces.

---
+++ src/cdiofinal/server/MySQLRaavareBatchDAO.java
@@ -0,0 +1,50 @@
+package cdiofinal.server;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cdiofinal.shared.RaavareBatchDTO;
+import cdiofinal.shared.ReceptKompDTO;
+
+public class MySQLRaavareBatchDAO implements RaavareBatchDAO{
+
+	@Override
+	public RaavareBatchDTO getRaavareBatch(int raavarebatchId) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM raavarebatch WHERE raavarebatch_id = " + raavarebatchId + ";");
+		try{
+			if (!rs.first()) throw new DALException("Raavarebatchen med raavarebatchId " + raavarebatchId + " findes ikke");
+			return new RaavareBatchDTO(rs.getInt("raavarebatch_id"), rs.getInt("raavare_id"), rs.getInt("leverandoer_id"), rs.getDouble("maengde"));
+		}
+		catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public List<RaavareBatchDTO> getRaavarebatchList() throws DALException {
+		List<RaavareBatchDTO> list = new ArrayList<RaavareBatchDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM raavarebatch;");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new RaavareBatchDTO(rs.getInt("raavarebatch_id"), rs.getInt("raavare_id"), rs.getInt("leverandoer_id"), rs.getDouble("maengde")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
+	}
+
+	@Override
+	public void createRaavareBatch(RaavareBatchDTO raavarebatch) throws DALException {
+		Connector.doUpdate("INSERT INTO raavarebatch(raavarebatch_id, raavare_id, leverandoer_id, maengde) VALUES (" + raavarebatch.getRaavarebatchId() + ","
+				+ raavarebatch.getRaavareId() + "," + raavarebatch.getLeverandoerId() + "," + raavarebatch.getMaengde() + ");");
+	}
+
+	@Override
+	public void updateRaavareBatch(RaavareBatchDTO raavarebatch) throws DALException {
+		Connector.doUpdate("UPDATE raavarebatch SET raavare_id = '" + raavarebatch.getRaavareId() + "', leverandoer_id = '"
+				+ raavarebatch.getLeverandoerId() + "', maengde = '" + raavarebatch.getMaengde() + "' WHERE raavarebatch_id = " + raavarebatch.getRaavarebatchId() + ";");
+	}
+
+}
---
+++ src/cdiofinal/server/MySQLRankDAO.java
@@ -0,0 +1,53 @@
+package cdiofinal.server;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cdiofinal.shared.RankDTO;
+import cdiofinal.shared.ReceptDTO;
+
+public class MySQLRankDAO implements RankDAO{
+
+	@Override
+	public RankDTO getRank(int titel) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM rank WHERE titel = " + titel + ";");
+		try {
+			if(!rs.first()) throw new DALException("Ranken med titel " + titel + " findes ikke");
+	    	return new RankDTO (rs.getInt("titel"), rs.getString("rank"));
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public List<RankDTO> getRankList() throws DALException {
+		List<RankDTO> list = new ArrayList<RankDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM rank;");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new RankDTO(rs.getInt("titel"), rs.getString("rank")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
+	}
+
+	@Override
+	public void createRank(RankDTO rank) throws DALException {
+		Connector.doUpdate(
+				"INSERT INTO rank(titel, rank) VALUES " +
+				"(" + rank.getTitel() + ", '" + rank.getRank() + "');"
+			);
+	}
+
+	@Override
+	public void updateRank(RankDTO rank) throws DALException {
+		Connector.doUpdate(
+				"UPDATE rank SET rank = '" + rank.getRank() + "' WHERE titel = " + rank.getTitel()
+				+ ";");
+	}
+
+}
