bdb702079167875b7a563279a3cdad85a73894e0
Thomas Bech Madsen <thomasbm@live.dk>
2016-06-08 20:13:47 +0200

    Merged CDIO_FINAL.java and LoginScreen.java

--- src/cdiofinal/client/Central.java
+++ src/cdiofinal/client/Central.java
@@ -13,7 +13,13 @@ import com.google.gwt.user.client.ui.TextBox;
 import cdiofinal.server.DALException;
 import cdiofinal.server.MySQLAnsatDAO;
 
-public class LoginScreen implements EntryPoint{
+public class Central implements EntryPoint{
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
 	@Override
 	public void onModuleLoad() {
@@ -32,8 +38,30 @@ public class LoginScreen implements EntryPoint{
 				if(!idTB.getText().isEmpty() || !passwordTB.getText().isEmpty()){
 					try{
 						if(database.getAnsat(idTB.getText()).getPassword() == passwordTB.getText()){
-							//login success
-							//Load CDIO_FINAL.java
+							RootPanel container = RootPanel.get("options");
+							String[] buttons = {"List users", "Create new user", "Tilføj råvare"};
+							final Composite[] compositeWidgets = {new ListUsersClickHandler(), new NewUserClickhandler(), new NewUserClickhandler(), new NewRaavareClickHandler(), new NewRaavareBatchClickHandler()};
+							ClickHandler[] clickHandlers = new ClickHandler[compositeWidgets.length];
+							for (int i = 0; i < compositeWidgets.length; i++) {
+								final int constant = i;
+								clickHandlers[i] = new ClickHandler() {
+
+									@Override
+									public void onClick(ClickEvent event) {
+										RootPanel panel = RootPanel.get("contents");
+										panel.clear();
+										Composite widget = compositeWidgets[constant];
+										panel.add(widget);
+									}
+									
+								};
+							}
+							for(int i=0;i<buttons.length;++i)
+							{
+								PushButton t = new PushButton(buttons[i]);
+								t.addClickHandler(clickHandlers[i]);
+								container.add(t);
+							}
 						}
 						else{
 							Window.alert("Wrong ID or password");
