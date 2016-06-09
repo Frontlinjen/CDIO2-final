dc35777f5a385f5e40a786b117e9084d77be71e8
SenadBegovic <senadbegovic2500@hotmail.com>
2016-06-09 15:44:06 +0200

    Fixed logic in NewProduktbatchComposite and relatedd classes

---
+++ src/cdiofinal/client/NewProduktbatchComposite.java
@@ -0,0 +1,31 @@
+package cdiofinal.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+
+public class NewProduktbatchComposite extends Composite{
+	interface NewProduktbatchUIBinder extends UiBinder<Widget, NewProduktbatchComposite> {}
+	private static NewProduktbatchUIBinder newProduktBatchUiBinder = GWT.create(NewProduktbatchUIBinder.class);
+	@UiField TextBox idBox;
+	@UiField ListBox statusBox;
+	@UiField TextBox recIdBox;
+	@UiField Label statusField;
+	NewProduktbatchComposite() {
+		initWidget(newProduktBatchUiBinder.createAndBindUi(this));
+	}
+	
+	@UiHandler("idBox")
+	public void onCprChanged(KeyPressEvent e)
+	{
+		statusField.setText(Character.toString(e.getCharCode()));
+	}
+}
