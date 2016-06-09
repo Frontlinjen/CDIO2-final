f3af64300993d1b6205e6fd55762c0f33692dfdb
Hjorthen <Sebastian@10.16.101.25>
2016-06-09 13:18:26 +0200

    Added loginscreen

--- src/cdiofinal/client/LoginScreen.java
+++ src/cdiofinal/client/LoginScreen.java
@@ -1,25 +1,39 @@
 package cdiofinal.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PushButton;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-import cdiofinal.server.DALException;
-import cdiofinal.server.MySQLAnsatDAO;
-
-public class LoginScreen implements EntryPoint{
+public class LoginScreen extends Composite{
+	interface LoginUIBinder extends UiBinder<Widget, LoginScreen> {}
+	private static LoginUIBinder loginUiBinder = GWT.create(LoginUIBinder.class);
+	@UiField Button submit;
+	@UiField TextBox username;
+	@UiField TextBox password;
 	
-
-	@Override
-	public void onModuleLoad() {
+	public LoginScreen(){
+		initWidget(loginUiBinder.createAndBindUi(this));
+	}
 	
-		
+	@UiHandler("submit")
+	public void onSubmit(ClickEvent e)
+	{
+		String pass = password.getText();
+		String usr = username.getText();
+		//Verify login here
+		//RootPanel.get("contents").clear();
+		//RootPanel.get("contents").add(new MainPage());
 	}
 
 }
