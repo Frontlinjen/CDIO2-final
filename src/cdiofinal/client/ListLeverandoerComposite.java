d4f7fce9a553be193ea8959fb8aac53db01596f6
Mikkel Leth <mkleth10@gmail.com>
2016-06-10 13:47:26 +0200

    Added dialogbox for leverandoercomposite

--- src/cdiofinal/client/ListLeverandoerComposite.java
+++ src/cdiofinal/client/ListLeverandoerComposite.java
@@ -5,8 +5,10 @@ import com.google.gwt.cell.client.ButtonCell;
 import com.google.gwt.cell.client.EditTextCell;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.client.Window;
@@ -14,6 +16,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
 import com.google.gwt.user.client.rpc.InvocationException;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
 
@@ -37,6 +40,17 @@ public class ListLeverandoerComposite extends Composite implements AsyncCallback
 	{
 		initWidget(listLeverandoerUiBinder.createAndBindUi(this));
 	}
+	
+	@UiHandler("newElement")
+	public void onClick(ClickEvent e)
+	{
+		NewLeverandoerComposite comp = new NewLeverandoerComposite();
+		DialogBox d = new DialogBox();
+		d.add(comp);
+		d.center();
+		d.show();
+	}
+	
 	public List<LeverandoerDTO> getLayoutList() { //TODO: Show users when clicked
 		vPanel = new CellTable<LeverandoerDTO>();
 		Column<LeverandoerDTO, String> IDColumn = getIDColumn();
