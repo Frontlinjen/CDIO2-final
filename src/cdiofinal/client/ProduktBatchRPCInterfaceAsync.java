f1dd4b06f7c8804d6b829a91774c7bf140658667
Mikkel Leth <mkleth10@gmail.com>
2016-06-12 10:43:27 +0200

    Fixed bug

--- src/cdiofinal/client/ProduktBatchRPCInterfaceAsync.java
+++ src/cdiofinal/client/ProduktBatchRPCInterfaceAsync.java
@@ -6,6 +6,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface ProduktBatchRPCInterfaceAsync {
 	public void getProduktBatch(int pb_id, String token, AsyncCallback<ProduktBatchDTO> callback);
 	public void getProduktBatchList(String token, AsyncCallback<ProduktBatchDTO[]> callback);
-	public void createProduktBatch(ProduktBatchDTO pb, String token, AsyncCallback<Integer> callback);
+	public void createProduktBatch(ProduktBatchDTO pb, String token, AsyncCallback<ProduktBatchDTO> callback);
 	public void updateProduktBatch(ProduktBatchDTO pb, String token, AsyncCallback<Integer> callback);
 }
--- src/cdiofinal/client/RaavareRPCInterfaceAsync.java
+++ src/cdiofinal/client/RaavareRPCInterfaceAsync.java
@@ -5,8 +5,8 @@ import cdiofinal.shared.RaavareDTO;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface RaavareRPCInterfaceAsync {
-	public void getRaavare(int raavareId, String token AsyncCallback<RaavareDTO> callback);
+	public void getRaavare(int raavareId, String token, AsyncCallback<RaavareDTO> callback);
 	public void getRaavareList(String token, AsyncCallback<RaavareDTO[]> callback);
-	public void createRaavare(RaavareDTO raa, String token, AsyncCallback<Integer> callback);
+	public void createRaavare(RaavareDTO raa, String token, AsyncCallback<RaavareDTO> callback);
 	public void updateRaavare(RaavareDTO raa, String token, AsyncCallback<Integer> callback);
 }
