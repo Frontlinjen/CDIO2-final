bed10d4468d96c44b085c3df0a6aeb8cf0d376a9
Mikkel Leth <mkleth10@gmail.com>
2016-06-12 13:04:07 +0200

    Added logic to the login system.
    To login, a user must write the correct credentials.

--- src/cdiofinal/client/LoginRPCInterface.java
+++ src/cdiofinal/client/LoginRPCInterface.java
@@ -1,10 +1,11 @@
 package cdiofinal.client;
 
+import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
+import java.lang.Exception;
 import cdiofinal.shared.DALException;
 
 @RemoteServiceRelativePath("login")
-public interface LoginRPCInterface {
+public interface LoginRPCInterface extends RemoteService{
 	String getLoginToken(int cpr, String password) throws DALException, Exception;
 }
--- src/cdiofinal/client/LoginScreen.java
+++ src/cdiofinal/client/LoginScreen.java
@@ -5,18 +5,24 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-public class LoginScreen extends Composite{
-	interface LoginUIBinder extends UiBinder<Widget, LoginScreen> {}
+import cdiofinal.shared.FieldVerifier;
+
+public class LoginScreen extends Composite  implements AsyncCallback<String>{
+	interface LoginUIBinder extends UiBinder<Widget, LoginScreen>{}
 	private static LoginUIBinder loginUiBinder = GWT.create(LoginUIBinder.class);
+	final LoginRPCInterfaceAsync database = (LoginRPCInterfaceAsync)GWT.create(LoginRPCInterface.class);
 	@UiField Button submit;
 	@UiField TextBox username;
 	@UiField TextBox password;
+	@UiField Label feedback;
 	
 	public LoginScreen(){
 		initWidget(loginUiBinder.createAndBindUi(this));
@@ -25,9 +31,36 @@ public class LoginScreen extends Composite{
 	@UiHandler("submit")
 	public void onSubmit(ClickEvent e)
 	{
+		feedback.setText("Waiting for server..");
 		String pass = password.getText();
 		String usr = username.getText();
-		//Verify login here
+		try{
+			int cpr = Integer.parseInt(usr);
+			if(!FieldVerifier.isValidCpr(cpr))
+			{
+				feedback.setText("CPR skal v\u00E6re 10 langt.");
+				return;
+			}
+			database.getLoginToken(cpr, pass, this);
+		} 
+		catch(NumberFormatException ex)
+		{
+			feedback.setText("CPR skal v\u00E6re et tal.");
+		}
+	}
+
+	@Override
+	public void onFailure(Throwable caught) {
+		System.out.println("Failed to login");
+		feedback.setText("Brugernavn eller password er forkert! Pr\u00F8v igen.");
+		
+	}
+
+	@Override
+	public void onSuccess(String result) {
+		System.err.println(result);
+		Token.setToken(result);
+		feedback.setText("Success!");
 		RootPanel.get("contents").clear();
 		RootPanel.get("contents").add(new MainPage());
 	}
