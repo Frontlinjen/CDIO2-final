4b58552f76b33fa8024394ee268d06719fdba9e8
SenadBegovic <senadbegovic2500@hotmail.com>
2016-06-13 10:58:25 +0200

    Added validation and error messages in ReceptKomonent class

--- src/cdiofinal/server/ReceptKomponentRPCServlet.java
+++ src/cdiofinal/server/ReceptKomponentRPCServlet.java
@@ -2,63 +2,61 @@ package cdiofinal.server;
 
 import java.util.List;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
 import cdiofinal.client.ReceptKomponentRPCInterface;
 import cdiofinal.shared.DALException;
-import cdiofinal.shared.InsufficientAccessException;
 import cdiofinal.shared.ReceptKompDTO;
 
-public class ReceptKomponentRPCServlet extends RemoteServiceServlet implements ReceptKomponentRPCInterface{
+public class ReceptKomponentRPCServlet extends ValidationServlet implements ReceptKomponentRPCInterface{
 	private static final long serialVersionUID = 1L;
 	MySQLReceptKompDAO receptKompDAO = new MySQLReceptKompDAO();
-	
+
 	@Override
 	public ReceptKompDTO getReceptKomp(int recId, int raavareId, String token) throws Exception{
 		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new InsufficientAccessException("Invalid token. Please refresh the page and login again.");
-			return receptKompDAO.getReceptKomp(recId, raavareId);
+			if(isValid(token, 2))
+				return receptKompDAO.getReceptKomp(recId, raavareId);
 		} catch (DALException e) {
-			throw new DALException("An error occoured when getting a receptkomp. Please contact your sysadmin.");
+			throw new DALException(gettingError("recept component"));
 		}
+		return null;
 	}
 
 	@Override
 	public ReceptKompDTO[] getReceptKompList(String token) throws Exception{
 		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new DALException("Invalid token");
-			List<ReceptKompDTO> receptkomp = receptKompDAO.getReceptKompList();
-			ReceptKompDTO[] receptKompArray = new ReceptKompDTO[receptkomp.size()];
-			return receptkomp.toArray(receptKompArray);
+			if(isValid(token, 2)){
+				List<ReceptKompDTO> receptkomp = receptKompDAO.getReceptKompList();
+				ReceptKompDTO[] receptKompArray = new ReceptKompDTO[receptkomp.size()];
+				return receptkomp.toArray(receptKompArray);
+			}
 		} catch (DALException e) {
-			throw new DALException("An error occoured when getting receptkomp list. Please contact your sysadmin.");
-		}	
+			throw new DALException("recept component");
+		}
+		return null;	
 	}
 
 	@Override
 	public ReceptKompDTO createReceptKomp(ReceptKompDTO recKomp, String token) throws Exception{
 		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new DALException("Invalid token");
-			if(receptKompDAO.createReceptKomp(recKomp)!=0){
-				return recKomp;
+			if(isValid(token, 2)){
+				if(receptKompDAO.createReceptKomp(recKomp)!=0){
+					return recKomp;
+				}
 			}
-			} catch (DALException e){
-				throw new DALException("An error occoured when creating a receptkomp. Please contact your sysadmin.");
-			}
-			return null;
+		} catch (DALException e){
+			throw new DALException(creatingError("recept component"));
+		}
+		return null;
 	}
 
 	@Override
 	public Integer updateReceptKomp(ReceptKompDTO recKomp, String token) throws Exception{
 		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new DALException("Invalid token");
-			return receptKompDAO.updateReceptKomp(recKomp);
-			} catch (DALException e){
-				throw new DALException("An error occoured when updating a receptkomp. Please contact your sysadmin.");
-			}
+			if(isValid(token, 2))
+				return receptKompDAO.updateReceptKomp(recKomp);
+		} catch (DALException e){
+			throw new DALException(updatingError("recept component"));
+		}
+		return null;
 	}
 }
