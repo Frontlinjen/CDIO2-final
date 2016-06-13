c619b55e6f9cf67e257f965455f442900ef801ba
SenadBegovic <senadbegovic2500@hotmail.com>
2016-06-13 11:09:09 +0200

    Updated ReceptServlet with validation methods and error messages

--- src/cdiofinal/server/ReceptRPCServlet.java
+++ src/cdiofinal/server/ReceptRPCServlet.java
@@ -1,69 +1,67 @@
 package cdiofinal.server;
 import java.util.List;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
 import cdiofinal.client.ReceptRPCInterface;
 import cdiofinal.server.MySQLReceptDAO;
 import cdiofinal.shared.DALException;
 import cdiofinal.shared.FieldVerifier;
-import cdiofinal.shared.InsufficientAccessException;
 import cdiofinal.shared.ReceptDTO;
 
-public class ReceptRPCServlet extends RemoteServiceServlet implements ReceptRPCInterface {
+public class ReceptRPCServlet extends ValidationServlet implements ReceptRPCInterface {
 
 	private static final long serialVersionUID = 1L;
 	MySQLReceptDAO receptDAO = new MySQLReceptDAO();
-	
-	
+
+
 	@Override
 	public ReceptDTO getRecept(int recept_id, String token) throws Exception{
 		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new InsufficientAccessException("Invalid token. Please refresh the page and login again.");
-			return receptDAO.getRecept(recept_id);
+			if(isValid(token, 2))
+				return receptDAO.getRecept(recept_id);
 		} catch (DALException e) {
-			throw new DALException("An error occoured when getting a recept. Please contact your sysadmin.");
+			throw new DALException(gettingError("recept"));
 		}
+		return null;
 	}
 
 	@Override
 	public ReceptDTO[] getReceptList(String token) throws Exception{
-					
-					try {
-						if(TokenHandler.getInstance().validateToken(token)==null)
-							throw new DALException("Invalid token");
-						List<ReceptDTO> recept = receptDAO.getReceptList();
-						ReceptDTO[] receptArray = new ReceptDTO[recept.size()];
-						return recept.toArray(receptArray);
-					} catch (DALException e) {
-						throw new DALException("An error occoured when getting recept list. Please contact your sysadmin.");
-					}			
+
+		try {
+			if(isValid(token, 2)){
+				List<ReceptDTO> recept = receptDAO.getReceptList();
+				ReceptDTO[] receptArray = new ReceptDTO[recept.size()];
+				return recept.toArray(receptArray);
+			}
+		} catch (DALException e) {
+			throw new DALException(gettingListError("recept"));
+		}
+		return null;			
 	}
 
 	@Override
 	public ReceptDTO createRecept(ReceptDTO ans, String token) throws Exception{
 		if(FieldVerifier.isValidId(ans.getReceptId())==true || FieldVerifier.isValidName(ans.getReceptNavn()) == true)
-		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new DALException("Invalid token");
-			if(receptDAO.createRecept(ans)!=0){
-				return ans;
+			try {
+				if(isValid(token, 2)){
+					if(receptDAO.createRecept(ans)!=0){
+						return ans;
+					}
+				}
+			} catch (DALException e){
+				throw new DALException(creatingError("recept"));
 			}
-		} catch (DALException e){
-			throw new DALException("An error occoured when creating a recept. Please contact your sysadmin.");
-		}
 		return null;
 	}
 
 	@Override
 	public Integer updateRecept(ReceptDTO ans, String token) throws Exception{
 		try {
-			if(TokenHandler.getInstance().validateToken(token)==null)
-				throw new DALException("Invalid token");
-			return receptDAO.updateRecept(ans);
-			} catch (DALException e){
-				throw new DALException("An error occoured when updating a recept. Please contact your sysadmin.");
-			}
+			if(isValid(token, 2))
+				return receptDAO.updateRecept(ans);
+		} catch (DALException e){
+			throw new DALException(updatingError("recept"));
+		}
+		return null;
 	}
 }
