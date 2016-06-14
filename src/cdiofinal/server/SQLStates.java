e5399a36438c1059692753af1ba3b93b7eb4d2ef
SenadBegovic <senadbegovic2500@hotmail.com>
2016-06-14 16:08:51 +0200

    Added more helper metods

--- src/cdiofinal/server/SQLStates.java
+++ src/cdiofinal/server/SQLStates.java
@@ -12,4 +12,20 @@ public class SQLStates {
 	{
 		return (e == INTEGRITY_FAILED) || (e == DUPLICATE_PRIMARY);
 	}
+	public static boolean isIntegrityFailure(String s)
+	{
+		return isIntegrityFailure(Integer.parseInt(s));
+	}
+	public static boolean isIntegrityFailure(int e)
+	{
+		return e==INTEGRITY_FAILED;
+	}
+	public static boolean isDuplicateFailure(String s)
+	{
+		return isDuplicateFailure(Integer.parseInt(s));
+	}
+	public static boolean isDuplicateFailure(int e)
+	{
+		return e==DUPLICATE_PRIMARY;
+	}
 }
