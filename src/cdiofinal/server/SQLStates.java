08f3a6fcf1cfe7ef9a644940227e13e5e33f87cd
SenadBegovic <senadbegovic2500@hotmail.com>
2016-06-14 16:04:25 +0200

    Added functionality for duplicate key

--- src/cdiofinal/server/SQLStates.java
+++ src/cdiofinal/server/SQLStates.java
@@ -2,4 +2,14 @@ package cdiofinal.server;
 
 public class SQLStates {
 	public static final int INTEGRITY_FAILED = 23000;
+	public static final int DUPLICATE_PRIMARY = 1062;
+	public static boolean isRecoverable(String s)
+	{
+		int e = Integer.parseInt(s);
+		return isRecoverable(e);
+	}
+	public static boolean isRecoverable(int e)
+	{
+		return (e == INTEGRITY_FAILED) || (e == DUPLICATE_PRIMARY);
+	}
 }
