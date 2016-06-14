3a4accc808dd0e6220e0cbda2e8ce3097f10fbbc
Hjorthen <Sebastian@10.16.101.25>
2016-06-14 17:28:14 +0200

    Updated DAO'er for better Exception handling

--- src/cdiofinal/server/MySQLProduktBatchKompDAO.java
+++ src/cdiofinal/server/MySQLProduktBatchKompDAO.java
@@ -9,52 +9,103 @@ import cdiofinal.server.Connector;
 import cdiofinal.server.ProduktBatchKompDAO;
 import cdiofinal.shared.DALException;
 import cdiofinal.shared.ProduktBatchKompDTO;
+import cdiofinal.shared.RaavareBatchDTO;
 
 public class MySQLProduktBatchKompDAO implements ProduktBatchKompDAO {
 
 	@Override
 	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int raavarebatchId) throws DALException {
+		 try {
 		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomponent WHERE pb_id = " + pbId + " AND raavarebatch_id = " + raavarebatchId + ";");
-	    try {
-	    	if (!rs.first()) throw new DALException("Produktbatchkomponenten med pb_id'et " + pbId + " og raavarebatch_id " + raavarebatchId + " findes ikke");
+	   
+	    	if (!rs.first())
+	    		return null;
 	    	return new ProduktBatchKompDTO (rs.getInt("pb_id"), rs.getInt("raavarebatch_id"), rs.getDouble("tara"), 
 	    			   rs.getDouble("netto"), rs.getString("cpr"));
 	    }
-	    catch (SQLException e) {throw new DALException(e); }
+	    catch (SQLException e) {
+	    	throw new DALException(e.getMessage()); 
+	    	}
 	}
 
 	@Override
 	public List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
 		List<ProduktBatchKompDTO> list = new ArrayList<ProduktBatchKompDTO>();
-		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomponent;");
 		try
 		{
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomponent;");
+		
 			while (rs.next()) 
 			{
 				list.add(new ProduktBatchKompDTO(rs.getInt("pb_id"), rs.getInt("raavarebatch_id"), 
 						 rs.getDouble("tara"), rs.getDouble("netto"), rs.getString("cpr")));
 			}
 		}
-		catch (SQLException e) { throw new DALException(e); }
+		catch (SQLException e) { 
+			throw new DALException(e.getMessage()); 
+			}
 		return list;
 	}
 
 	@Override
 	public int createProduktBatchKomp(ProduktBatchKompDTO pbkomp) throws DALException {
+		try
+		{
 		return Connector.doUpdate(
 				"INSERT INTO produktbatchkomponent(pb_id, raavarebatch_id, tara, netto, cpr) VALUES ("
 				+ pbkomp.getPbId() + ", " + pbkomp.getRaavarebatchId() + ", '" + pbkomp.getTara() + "', '" 
 				+ pbkomp.getNetto()	+ "', '" + pbkomp.getCpr() + "');"
 			);
+		}
+		catch(SQLException e)
+		{
+			if(SQLStates.isDuplicateFailure(e.getSQLState()))
+			{
+				throw new DALException("Produktbatchkomponenten eksisterer allerede!");
+			}
+			else if(SQLStates.isIntegrityFailure(e.getSQLState()))
+					{
+						throw new DALException(getIntegrityError(pbkomp));
+					}
+			//Cant handle this error. Pass it on to the user. 
+			throw new DALException(e.getMessage());
+		}
 	}
-
+	
+	
+	private String getIntegrityError(ProduktBatchKompDTO pbkomp) throws DALException
+	{
+		RaavareBatchDAO raavareBatches = new MySQLRaavareBatchDAO();
+		ProduktBatchDAO productBatches = new MySQLProduktbatchDAO();
+		if(raavareBatches.getRaavareBatch(pbkomp.getRaavarebatchId())==null)
+		{
+			return "Raavarebatchen eksisterer ikke";
+		}
+		else if(productBatches.getProduktBatch(pbkomp.getPbId())==null)
+		{
+			return "ProduktBatchen eksisterer ikke";
+		}
+		return "Unknown integrity error";
+	}
 	@Override
 	public int updateProduktBatchKomp(ProduktBatchKompDTO pbkomp) throws DALException {
+		try
+		{
 		return Connector.doUpdate(
 				"UPDATE produktbatchkomponent SET tara = '" + pbkomp.getTara() + "', netto = '" + pbkomp.getNetto() +
 				"', cpr = '" + pbkomp.getCpr() + "' WHERE pb_id = " + pbkomp.getPbId() + " AND raavarebatch_id = " 
 						+ pbkomp.getRaavarebatchId() + ";"
-				);		
+				);	
+		}
+		catch(SQLException e)
+		{
+			if(SQLStates.isIntegrityFailure(e.getSQLState()))
+					{
+						throw new DALException(getIntegrityError(pbkomp));
+					}
+			//Cant handle this error. Pass it on to the user. 
+			throw new DALException(e.getMessage());
+		}
 	}
 
 }
